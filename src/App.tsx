9cefbdb77d6003c21304cb06b78c78236e4bb220
klaidas1591 <klaiduxlt@gmail.com>
2023-02-10 21:48:35 +0200

    last version

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './styles/style.css';
+import './styles/navigation.css';
+import './styles/countriesList.css';
+import './styles/pagination.css';
+import { useEffect, useState } from 'react';
+import CountryList from './components/CountriesList';
+import { Fetch, Filter, Sort } from './functions';
+import { ICountryData, IDATA, ISort } from './interfaces';
+import Nav from './components/nav';
 
 function App() {
+  const [dataCopy, setDataCopy] = useState<ICountryData[]>([]);
+  const [filteredData, setFilteredData] = useState<ICountryData[]>([]);
+  const [order, setOrder] = useState("DSC");
+
+  useEffect(() => {
+    fetchCountries();
+  }, [])
+
+  /*    fetching data(countries)     */
+  const fetchCountries = async() => {
+    const {countries, error }:IDATA = await Fetch("https://restcountries.com/v2/all?fields=name,region,area");
+    setDataCopy(countries);
+    setFilteredData(countries);
+    if(error) alert(error)
+    console.log("data fetched(fetchCountries)")
+    console.log(countries);
+  }
+
+  /*    FILTER     */
+  const handleFilter = async(filter:string) => {
+    const {newFilteredData} = Filter(filter, filteredData as ICountryData[], dataCopy as ICountryData[]);
+    setFilteredData(newFilteredData);
+    console.log("filter: " + filter);
+  }
+
+  /*    SORTING     */
+  const sorting = async ()=> {
+    const { sorted, newOrder }:ISort = await Sort(filteredData as ICountryData[], order);
+    setFilteredData(sorted);
+    setOrder(newOrder);
+    console.log("sort order: " + order);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="content">
+      <h1>{filteredData ? "Reiz Tech Task" : "data error"}</h1>
+      <Nav handleFilter = {handleFilter} sorting = {sorting} />
+      {filteredData.length ?
+        <CountryList filteredData={filteredData} />
+        :
+        <div className='loading'>Loading data</div>
+        }
     </div>
   );
 }
-
 export default App;
